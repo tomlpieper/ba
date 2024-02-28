0105efd9733c11d03a015a9fa91c89dfa50625be
Tom Pieper <tpieper@uni-osnabrueck.de>
2024-02-28 13:16:42 +0100

    working version of max token eval output

--- v1/main.py
+++ v1/main.py
@@ -70,10 +70,10 @@ if __name__ == "__main__":
 
     args = Seq2SeqTrainingArguments(
                 predict_with_generate=True,
-                generation_max_length=400,
+                generation_max_length=100,
                 evaluation_strategy="steps",
-                eval_steps=5,
-                save_steps=5,
+                eval_steps=500,
+                save_steps=1000,
                 warmup_steps=500,
                 per_device_train_batch_size=train_batch_size,
                 per_device_eval_batch_size=eval_batch_size,
@@ -82,7 +82,7 @@ if __name__ == "__main__":
                 output_dir= weights_path + "outputs",
                 fp16=use_cuda,
                 logging_dir=weights_path + "logs",
-                logging_steps=5,
+                logging_steps=500,
                 load_best_model_at_end=True,     # Load the best model when finished training (default metric is loss)
                 metric_for_best_model='loss',
                 # gradient_accumulation_steps=2
